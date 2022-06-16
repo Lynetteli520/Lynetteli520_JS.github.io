46cb90e717c6915417274963098c568154f8ef3c
Lynetteli520 <lynettee@umich.edu>
2022-06-15 18:50:41 -0700

    change to dblclick event

--- js/waiver.js
+++ js/waiver.js
@@ -16,7 +16,7 @@ var figures = document.getElementsByTagName("figure");
 var i;
 
 for(i = 0; i < figures.length; i++) {
-    figures[4].addEventListener("click", myFunction);
+    figures[4].addEventListener("dblclick", myFunction);
     function myFunction() {
         figures[4].setAttribute("style", "visibility:hidden");
 
@@ -47,21 +47,6 @@ for(i = 0; i < captions.length; i++) {
     }
 }
 
-
-// Use an external JavaScript listener to change the font of the first figure caption to Cursive 
-// when the figure caption is in focus and restore the font to “Times” when the focus state is over.  
-//  Note you must use JavaScript to accomplish this, not the CSS hover state.  
-//  You will need to use JavaScript to modify the html file to accomplish this task.
-
-// var caps = document.getElementsByTagName("figcaption");
-// var i;
-
-// for(i = 0; i < caps.length; i++) {
-//     // caps[i].className += "one-third";
-//     caps[0].setAttribute("style", "font-family:Cursive");
-// }
-
-// event listener for keyup
 var cpt = document.getElementsByTagName("figcaption");
 for(i = 0; i < cpt.length; i++) {
         cpt[0].setAttribute("tabindex", "1");
@@ -80,22 +65,3 @@ for(i = 0; i < cpt.length; i++) {
     }
 
 }
-
-// function checkTabPress(key) {
-//     key = key || event;
-//     if (key.keyCode == 9) {
-//         tabindex="0"
-//         for(i = 0; i < cpt.length; i++) {
-//             function key() {
-//                 cpt[0].setAttribute("style", "font-family:Cursive");
-//                 alert('e')
-//             }
-
-//         }
-        
-
-//     }
-// }
-
-// var body = document.querySelector('body');
-// body.addEventListener('keyup', checkTabPress);
